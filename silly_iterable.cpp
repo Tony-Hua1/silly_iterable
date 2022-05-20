6cf32fabf1735d0b039af4ca763faef76f50645e
Tony Hua <tony7068@gmail.com>
2022-05-20 12:18:59 -0700

    Create iterator to check for leap years

--- silly_iterable.cpp
+++ silly_iterable.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <typeinfo>
 
+#include <math.h>
+#include <cmath>
+#include <tgmath.h>
+#include <limits>
+#include <bits/stdc++.h>
+
+using std::ilogb;
+
 using std::cout;
 using std::endl;
 
@@ -11,8 +19,41 @@ bool is_leap_year(long year) {
 	return false;
 }
 
+class LeapYears {
+	long max_year;
+public:
+	class Iterator {
+		long curr_year;
+	public:
+		Iterator(long curr_year) : curr_year(curr_year) {}
 
+		bool operator!=(const Iterator& other) const {
+			return curr_year != other.curr_year;
+		}
 
+		Iterator& operator++() {
+			for (++curr_year; !is_leap_year(curr_year); ++curr_year);
+			return *this;
+		}
+
+		long operator*() const {
+			return curr_year;
+		}
+	};
+
+	LeapYears(long max_year) {
+		for (; !is_leap_year(max_year); --max_year);
+		this->max_year = max_year;
+	}
+
+	Iterator begin() const {
+		return Iterator(1900);
+	}
+
+	Iterator end() const {
+		return Iterator(max_year);
+	}
+};
 
 int main() {
 	// for (int i = 1900; i < 2022; ++i) {
@@ -21,12 +62,8 @@ int main() {
 	// 	}
 	// }
 
-	const int foo = 1;
-	auto x = foo;
-
-	cout << typeof(x) << endl;
-
-
-
+	for (long x : LeapYears(2022)) {
+		cout << x << endl;
+	}
 	return 0;
 }
